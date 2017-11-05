070dc180e30699887f10a6cf635f30076d0fc56e
mamatucci <mamatucci@gmail.com>
2017-11-05 17:06:02 +0100

    typo on
    unzip videos.zip  2> /dev/null and mv

--- convmp4tomp3batch.sh
+++ convmp4tomp3batch.sh
@@ -9,13 +9,13 @@ params2="-vn -ar 44100 -ac 2 -ab 192k -f mp3"
 #runtime params
 params=$params2
 #avoid mp4 in the names ;P
-targetFolder="/tmp/obar1/videos" 
+targetFolder="/tmp/obar1/videos"
 
 # get latest
 cd  $targetFolder
 rm * -f
 wget https://www.dropbox.com/sh/6qchq16vzkfp98z/AAB6v7o9XwDFFhtaSbN1q7dWa?dl=1 -O videos.zip
-unzip v  2> /dev/null
+unzip videos.zip  2> /dev/null
 rm videos.zip
 du . -hs
 cd -
@@ -23,9 +23,9 @@ cd -
 # process videos
 find $targetFolder -name "*.mp4" -print0 |
 sed -e "s/.mp4//g"  |
-while read -d $'\0' file; do 
-   ./ffmpeg-3.4-64bit-static/ffmpeg -y -i "$file.mp4" $params "$file.mp3" < /dev/null; 
+while read -d $'\0' file; do
+   ./ffmpeg-3.4-64bit-static/ffmpeg -y -i "$file.mp4" $params "$file.mp3" < /dev/null;
 done
 
-mv $targetFolder/*.mp3 ..
+mv $targetFolder/*.mp3 $targetFolder/..
 du $targetFolder/.. -hs
