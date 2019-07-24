22a8c14a35a6ca3acbc4e0c153d75a9b9b80d0b2
obar1 <387386+obar1@users.noreply.github.com>
2019-07-24 17:08:41 +0200

    Update and rename setup-windows10-ubuntu.md to setup-windows10-ubuntu.sh

---
+++ setup-windows10-ubuntu.sh
@@ -0,0 +1,11 @@
+#> yes it sucks when you have to work with `windows`
+#let s leverage the  https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+# curl
+sudo apt-get install -y curl
+
+# pip 
+sudo apt install python-pip
+
+# pyspark
+sudo pip install --upgrade pyspark
