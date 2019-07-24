7777a0f6b2ee4a5424618109e5f996da7fcfcaec
obar1 <387386+obar1@users.noreply.github.com>
2019-07-24 17:59:48 +0200

    Update setup-windows10-ubuntu.sh

--- setup-windows10-ubuntu.sh
+++ setup-windows10-ubuntu.sh
@@ -1,6 +1,8 @@
 #> yes it sucks when you have to work with `windows`
 #let s leverage the  https://docs.microsoft.com/en-us/windows/wsl/install-win10
 
+sudo apt-get update
+
 # curl
 sudo apt-get install -y curl
 
@@ -9,3 +11,6 @@ sudo apt install python3-pip
 
 # pyspark
 sudo pip3 install --upgrade pyspark
+
+# caliber
+sudo apt-get install calibre
